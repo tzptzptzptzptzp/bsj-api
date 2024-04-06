2638c1b5ae98e8341eee894ff2ba485b338d3c1b
tzptzptzptzptzp <git@tzp.pink>
2024-04-07 00:47:24 +0900

    ⚡️ サイズを指定できるように変更

--- src/app/api/bsj/route.ts
+++ src/app/api/bsj/route.ts
@@ -1,17 +1,32 @@
 import { NextRequest, NextResponse } from "next/server";
 import fs from "fs";
 import path from "path";
+import sharp from "sharp";
 
-export function GET(req: NextRequest): NextResponse {
+export async function GET(req: NextRequest): Promise<NextResponse> {
   const month = req.nextUrl.searchParams.get("month");
-  const width = req.nextUrl.searchParams.get("width");
-  const height = req.nextUrl.searchParams.get("height");
+  const size = req.nextUrl.searchParams.get("size");
+
+  const headers = new Headers();
+  headers.set("Content-Type", "image/png");
 
   const filePath = path.resolve(".", "public", "bisyojo_chan.png");
   const image = fs.readFileSync(filePath);
 
-  const headers = new Headers();
-  headers.set("Content-Type", "image/png");
+  if (size) {
+    try {
+      const resizeImage = await sharp(image)
+        .resize({
+          width: parseInt(size),
+          height: parseInt(size),
+          fit: "fill",
+        })
+        .toBuffer();
+      return new NextResponse(resizeImage, { status: 200, headers: headers });
+    } catch (err) {
+      console.error("Error:", err);
+    }
+  }
 
   return new NextResponse(image, { status: 200, headers: headers });
 }
