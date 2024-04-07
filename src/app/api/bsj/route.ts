4f38f02b9b9472ec80834e5532f8e3d3cd4248ec
tzptzptzptzptzp <git@tzp.pink>
2024-04-07 22:40:25 +0900

    ⚡️ 指定した月の画像を返すように変更

--- src/app/api/bsj/route.ts
+++ src/app/api/bsj/route.ts
@@ -3,6 +3,12 @@ import fs from "fs";
 import path from "path";
 import sharp from "sharp";
 
+const BASE_IMAGE = "bisyojo_chan_";
+
+const createImageName = (month: string) => {
+  return `${BASE_IMAGE}${month.padStart(2, "0")}.png`;
+};
+
 export async function GET(req: NextRequest): Promise<NextResponse> {
   const month = req.nextUrl.searchParams.get("month");
   const size = req.nextUrl.searchParams.get("size");
@@ -10,7 +16,9 @@ export async function GET(req: NextRequest): Promise<NextResponse> {
   const headers = new Headers();
   headers.set("Content-Type", "image/png");
 
-  const filePath = path.resolve(".", "public", "bisyojo_chan.png");
+  const imageName = month ? createImageName(month) : createImageName("0");
+
+  const filePath = path.resolve(".", "public", imageName);
   const image = fs.readFileSync(filePath);
 
   if (size) {
