5b0cce86e6ba59eaf5bac7e350d5662502f0a963
tzptzptzptzptzp <git@tzp.pink>
2025-06-25 02:12:32 +0900

    ✨ Codeコンポーネントにサイズプロパティを追加し、スタイルを改善

--- src/app/components/elements/Code.tsx
+++ src/app/components/elements/Code.tsx
@@ -1,6 +1,25 @@
-export const Code = ({ children }: { children: React.ReactNode }) => {
+import { clsx } from "clsx";
+
+type Props = {
+  children: React.ReactNode;
+  size?: "xs" | "sm" | "base" | "lg";
+};
+
+export const Code = ({ children, size = "sm" }: Props) => {
+  const sizeVariantClasses = {
+    xs: "text-[0.7rem] leading-none px-1.5 py-0.5",
+    sm: "text-sm  px-1.5 py-0.5",
+    base: "text-base px-2 py-1",
+    lg: "text-lg px-2.5 py-1.5",
+  };
+
   return (
-    <code className="bg-gray-100 dark:bg-gray-800 rounded px-1.5 py-0.5 text-sm">
+    <code
+      className={clsx(
+        "rounded bg-gray-100 dark:bg-gray-800",
+        sizeVariantClasses[size]
+      )}
+    >
       {children}
     </code>
   );
