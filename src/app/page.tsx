63d6fc223959cd5116520f5a81df67998866053a
tzptzptzptzptzp <git@tzp.pink>
2025-06-28 00:41:26 +0900

    ✨ サーバーサイドで画像データを取得

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,20 @@
 import { Index } from ".";
+import { getBsjImages } from "@/utils/api/getBsjImages.api";
 
-export default function Home() {
-  return <Index />;
+export default async function Home() {
+  const [defaultImages, allImages, currentImages, randomImages] =
+    await Promise.all([
+      getBsjImages("default"),
+      getBsjImages("all"),
+      getBsjImages("current"),
+      getBsjImages("random"),
+    ]);
+  return (
+    <Index
+      defaultImages={defaultImages}
+      allImages={allImages}
+      currentImages={currentImages}
+      randomImages={randomImages}
+    />
+  );
 }
