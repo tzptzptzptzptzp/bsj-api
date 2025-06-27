2627de83ab08d69028fb77e402bf6deb2996e0d5
tzptzptzptzptzp <git@tzp.pink>
2025-06-28 00:40:56 +0900

    🔧 画像設定を追加し、リモートパターンを定義

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "pub-c799e6d1bb0a429987fe4c059717f3e4.r2.dev",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
