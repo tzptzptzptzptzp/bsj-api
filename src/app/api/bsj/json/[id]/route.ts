32680593a5af360affd882be9ed45ee47bf45952
tzptzptzptzptzp <git@tzp.pink>
2025-06-27 22:34:50 +0900

    ⚡️ JSONを返すAPIを作成

---
+++ src/app/api/bsj/json/[id]/route.ts
@@ -0,0 +1,17 @@
+import { NextRequest, NextResponse } from "next/server";
+import { unstable_noStore as noStore } from "next/cache";
+import { getImageDataById } from "@/app/api/utils/getImageDataById";
+
+export async function GET(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  noStore();
+  const imageData = getImageDataById(params.id);
+
+  if (!imageData) {
+    return NextResponse.json({ error: "Not Found" }, { status: 404 });
+  }
+
+  return NextResponse.json(imageData);
+}
