bf8fecfb020e261b00db4e51a968c4887f039475
tzptzptzptzptzp <git@tzp.pink>
2025-07-02 14:46:47 +0900

    ✨ スクリーンサイズ設定を追加

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,12 +7,13 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    screens: {
+      s: { max: "639px" }, // モバイル端末サイズにのみ適用（639px以下）
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
     },
   },
   plugins: [],
